b22177390df021dd2590985fcc621065ed6ca77d
AldoJanuar <48519437+Januarrio@users.noreply.github.com>
2019-03-14 10:28:06 +0700

    Add files via upload

---
+++ 17090117_januarrio_kus_gunaldo.java
@@ -0,0 +1,7 @@
+public class 17090117_januarrio_kus_gunaldo {
+    public static void main(String[] args) {
+        System.out.println("Nama  : Januarrio Kus Gunaldo\n"
+                         + "NIM   : 17090117\n"
+                         + "Kelas : TI 4C");
+    }
+}
